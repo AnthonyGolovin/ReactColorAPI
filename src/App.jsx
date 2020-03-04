9d4e9570cc8bfcd17caaaac69934c9689a2baa10
Anthony Golovingit config --global user.name Anthony <AGOLOVIN97@live.com>
2020-03-03 17:29:51 -0800

    Add box components and container

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BoxCon from './container/box-container.jsx';
+
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <BoxCon></BoxCon>
+            </div>
+        )
+    }
+}
+export default App;
---
+++ src/component/box.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Box extends React.Component{
+    render(){
+        return(
+            <div>
+                <button>Color changer</button>
+            </div>
+        )
+    }
+}
+export default Box;
